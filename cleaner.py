8f9e34ce25463d70cf9463088f56e9a8d9f699d5
kkeelar <88795998+kkeelar@users.noreply.github.com>
2023-03-29 20:18:49 -0400

    Delete cleaner.py

--- cleaner.py
+++
@@ -1,9 +0,0 @@
-with open('accountsList.txt', 'r') as f:
-    lines = f.readlines()
-
-lines = list(set(lines))
-
-with open('FinalAccountList2.txt', 'a') as f:
-    for line in lines:
-        f.write(line)
-        
